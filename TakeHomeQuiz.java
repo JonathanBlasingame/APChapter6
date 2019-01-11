60fe3ee2696558ab1b55d9e0e121753001c89954
JonathanBlasingame <32464741+JonathanBlasingame@users.noreply.github.com>
2019-01-11 08:22:47 -0600

    Update TakeHomeQuiz.java

--- TakeHomeQuiz.java
+++ TakeHomeQuiz.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
 public class TakeHomeQuiz{
   public static void main(String[] args){
-    System.out.println("Enter some numbers between 0 and 100");
+    System.out.println("Enter some numbers between 0 and 100.");
+    System.out.println("Signal the end by entering a number out of that range.");
+    Scanner scan= new Scanner(System.in);
+    i= scan.nextInt();
+    while(i < 100){
+      if(i >= 1 && i <= 10){
+        
+      }
     }
   }
 }
